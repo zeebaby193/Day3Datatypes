6c76193aa7da2cdd567901db7aee263dab09cdb2
zeebaby193 <zainabenitan422@gmail.com>
2026-02-20 12:49:32 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+string fullName = "Muhammad Saheera";
+int age = 12;
+double height = 5.2;
+bool iLikeCoding = true;
+
+Console.WriteLine(fullName);
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(iLikeCoding);
